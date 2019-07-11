b1ceca11e210cdbcca145fc5f1dd305170931484
KEEN <kyh0927@live.co.kr>
2019-07-12 01:13:50 +0900

    gogo

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Head from 'next/head';
 import Link from 'next/link';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
     return (
         <>
-            <Link href="/about"><a>about</a></Link>
-            하세깃!
+            <Head>
+                <title>Node Bird</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.20.1/antd.min.css" />
+            </Head>
+            <AppLayout>
+                <div>Hello</div>
+            </AppLayout>
         </>
     );
 };
