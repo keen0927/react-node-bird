288cb9461f4af3b4013976c70152e0b15c0b1d96
keen <kyh0927@within.co.kr>
2019-07-17 13:40:28 +0900

    20190717

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,22 +1,36 @@
 
 self.addEventListener('install', event => {
-    console.log('V1 installing...');
+    console.log('인스톨 중...');
 
     // cache
     event.waitUntil(
-        caches.open('static-v1')
-            .then(cache => cache.add('https://keen0927.github.io/img/keen-logo__none.png'))
+        caches.open('static-v1').then(cache => {
+            return cache.addAll([
+                '',
+                '/frontend/serviceWorker.html',
+                'https://keen0927.github.io/img/keen-logo__none.png'
+            ])
+                .then(() => self.skipWaiting());
+        })
     );
 })
 
 self.addEventListener('activate', event => {
-    console.log('V1 now ready to handle fetches!');
+    event.waitUntil(self.clients.claim())
+    console.log('V1 준비됨!');
 })
 
 self.addEventListener('fetch', event => {
-    const url = new URL(event.request.url);
+    // const url = new URL(event.request.url);
 
-    if (url.origin == location.origin && ual.pathname == 'https://keen0927.github.io/img/keen-logo__none.png') {
-        event.respondWith(caches.match('https://keen0927.github.io/img/keen-logo__none.png'));
-    }
+    // if (url.origin == location.origin && url.pathname == 'https://keen0927.github.io/img/keen-logo__none.png') {
+    //     event.respondWith(caches.match('https://keen0927.github.io/img/keen-logo__none.png'));
+    // }
+    event.respondWith(
+        caches.open('static-v1')
+            .then(cache => cache.match(event.request, {ignoreSearch: true}))
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    )
 });
