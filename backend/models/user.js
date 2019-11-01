3d4adc5047a4c05ae7af14bb0580cfacc4d3e845
KEEN <kyh0927@live.co.kr>
2019-11-01 23:58:27 +0900

    191101

--- backend/models/user.js
+++ backend/models/user.js
@@ -18,8 +18,11 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   User.associate = (db) => {
-    db.User.hasMany(db.Post);
+    db.User.hasMany(db.Post, { as: 'Post '});
     db.User.hasMany(db.Comment);
+    db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked' });
+    db.User.belongsToMany(db.User, { through: 'Follow', as: 'Follower' }); 
+    db.User.belongsToMany(db.User, { through: 'Follow', as: 'Following' });
   };
 
   return User;
