ee3bf059b3145668ddebd709f817be95fe370d30
KEEN <kyh0927@live.co.kr>
2019-11-02 23:51:48 +0900

    191102

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const db = require('./models');
 const app = express();
 
+const userAPIRouter = require('./routes/user');
+const postAPIRouter = require('./routes/post');
+const postsAPIRouter  = require('./routes/posts');
+
 db.sequelize.sync();
 
-app.get('/', (req, res) => {
-  res.send('Hello Server');
-});
+app.use('/api/user', userAPIRouter);
+app.use('/api/post', postAPIRouter );
+app.use('/api/posts', postsAPIRouter);
 
-app.get('/about', (req, res) => {
-  res.send('Hello, about');
-});
 
 app.listen(3065, () => {
   console.log('hello server')
